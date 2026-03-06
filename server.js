d7e3a7886814bebfde37dada030a93c47712e78b
masaru14 <tikko14zawa@gmail.com>
2026-03-06 16:01:52 +0900

    fix public path

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
-app.get("/", (req,res)=>{
- res.sendFile(__dirname + "/index.html");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+ res.sendFile(path.join(__dirname,"public","index.html"));
 });
 
 let rooms = {};
